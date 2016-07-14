ec4e9b09926bff1fa61927b8cfa084218966751c
theoperatore <theoperatore@gmail.com>
2016-07-14 00:37:08 -0400

    basic websocket setup with action types!

--- server/server.js
+++ server/server.js
@@ -4,15 +4,22 @@ const express = require('express');
 const IO = require('socket.io');
 const debug = require('debug');
 
-const config = require('../config');
 const wssLog = debug('rockin:wss');
 const httpLog = debug('rockin:http');
+const config = require('../config');
+
+// prolly needs a rename?
+const initializeGame = require('../lib/game');
 
 const app = express();
 const io = IO(config.WSS_PORT);
+const rockin = initializeGame(io);
 
 io.on('connection', socket => {
-  wssLog('new client connected [ %s ] ( %s )', socket.id, Object.keys(io.sockets.sockets).length);
+  let count = Object.keys(io.sockets.sockets).length;
+  wssLog('client connected [ %s ] ( %s )', socket.id, count);
+
+  rockin.syncClient(socket);
 });
 
 app.use('*', (req, res, next) => {
@@ -20,7 +27,7 @@ app.use('*', (req, res, next) => {
   next();
 });
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/../public`));
 
 let server = app.listen(config.HTTP_PORT, () => {
   httpLog(`http server up on port ${config.HTTP_PORT}`);
