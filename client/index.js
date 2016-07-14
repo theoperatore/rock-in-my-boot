fce989289f2255137bb06160a9bbaf1391238f10
Andrew Petersen <kirbysayshi@gmail.com>
2016-07-14 00:18:26 -0400

    maybe rollup

--- client/index.js
+++ client/index.js
@@ -1,1 +1,20 @@
-'use strict';
+import io from 'socket.io-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('le go');
+
+const socket = io.connect(getSocketUrl());
+
+socket.on('message', data => console.log('data', data));
+socket.on('connect', data => console.log('connect', data));
+socket.on('connect_error', err => console.error(err));
+socket.on('connect_timeout', err => console.error(err));
+
+
+ReactDOM.render(<p>hello</p>, document.body.querySelector('#root'));
+
+function getSocketUrl() {
+  let [ protocol, url ] = document.location.origin.split(':');
+  return `${protocol}:${url}:${process.env.WSS_PORT}`;
+}
