aaa8bcb93d5658ffa65e3bcfa03a109e241a0f16
theoperatore <theoperatore@gmail.com>
2016-07-13 23:24:32 -0400

    inital web socket stuffs

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  HTTP_PORT: 9966,
+  WSS_PORT: 9967,
+};
--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const express = require('express');
+const IO = require('socket.io');
+const debug = require('debug');
+
+const config = require('../config');
+const wssLog = debug('rockin:wss');
+const httpLog = debug('rockin:http');
+
+const app = express();
+const io = IO(config.WSS_PORT);
+
+io.on('connection', socket => {
+  wssLog('new client connected [ %s ] ( %s )', socket.id, Object.keys(io.sockets.sockets).length);
+});
+
+app.use('*', (req, res, next) => {
+  httpLog(`[${req.method}] => ${req.originalUrl}`);
+  next();
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+let server = app.listen(config.HTTP_PORT, () => {
+  httpLog(`http server up on port ${config.HTTP_PORT}`);
+})
